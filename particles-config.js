cf2fcfb05fb191f6d9f7233164f1ddef3c891ec7
bexody <beex1996@hotmail.com>
2025-09-19 20:22:32 -0300

    Cambiar color de partículas

---
+++ particles-config.js
@@ -0,0 +1,12 @@
+particlesJS('particles-js', {
+  "particles": {
+    "number": { "value": 100, "density": { "enable": true, "value_area": 800 } },
+    "color": { "value": "#002AFF" },  // ← Color de partículas
+    "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" } },
+    "opacity": { "value": 0.5, "random": true, "anim": { "enable": true, "speed": 1, "opacity_min": 0.1 } },
+    "size": { "value": 3, "random": true, "anim": { "enable": true, "speed": 4, "size_min": 0.1 } },
+    "line_linked": { "enable": true, "distance": 150, "color": "#002AFF", "opacity": 0.4, "width": 1 },
+    "move": { "enable": true, "speed": 2, "direction": "none", "random": true, "straight": false, "out_mode": "out", "bounce": false }
+  },
+  "retina_detect": true
+});
